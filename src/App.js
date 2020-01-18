4aab1d38bd181d538ac0ba15d07f0a7b74c70b7b
Bbara <backdown959@gmail.com>
2020-01-18 17:22:26 +0300

    add componentDidUpdate

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('componentDidUpdate')
+    }
+
     render() {
         return (
             <div className="App">
